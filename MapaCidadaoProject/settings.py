556d5ea8a3cca9624e4d394d52b453a40218e112
Alexandre-Gama <gama.pereira2004@gmail.com>
2025-03-23 21:47:57 -0300

    Adicionado host do Render ao settings.py

--- MapaCidadaoProject/settings.py
+++ MapaCidadaoProject/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 # Em produção, definir DEBUG como False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mapacidadao.onrender.com']
 
 
 # Application definition
