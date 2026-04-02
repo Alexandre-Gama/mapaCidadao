4f2e5430f470871556427223e261b7464345d56a
Alexandre Gama Pereira <gamapereira04@gmail.com>
2026-04-01 22:06:12 -0300

    Ajuste Origin CSRF

--- MapaCidadaoProject/settings.py
+++ MapaCidadaoProject/settings.py
@@ -9,6 +9,8 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'http://localhost:8080').split(',')
+
 
 # Application definition
 
